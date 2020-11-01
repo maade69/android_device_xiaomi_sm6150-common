40240b687581ad89bbf86829fcb20331f36f443c
Arian <arian.kulmer@web.de>
2020-11-01 21:28:07 +0100

    sm6150-common: Don't append -qti to power HAL name
    Change-Id: Icaa436d9961bb7ba0e560b3d03da73752f1e15a0
    Signed-off-by: maade <p1nchp0d@bk.ru>

--- sm6150.mk
+++ sm6150.mk
@@ -271,7 +271,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service
 
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
