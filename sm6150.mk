f70ee767f8960ad68f206cbba540ffc340fc4b64
Rasenkai <rasenkai99@gmail.com>
2021-06-14 14:59:45 +0530

    sm6150-common: Use common QTI av component
    Change-Id: I283de6cc57fc83fa46c6981a9c5ae484488f93c0

--- sm6150.mk
+++ sm6150.mk
@@ -161,7 +161,6 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libavservices_minijail.vendor \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor \
     libOmxAacEnc \
@@ -219,6 +218,7 @@ TARGET_BOARD_PLATFORM := $(MSMSTEPPE)
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
+    av \
     bt \
     gps \
     perf \
